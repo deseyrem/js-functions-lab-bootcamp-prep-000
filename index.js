a1ee3c7c101a5914448b4433e710502df691275a
Deseyre M <deseyrem@gmail.com>
2018-03-02 14:40:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 // write your code below!
 function happyHolidays() {
-  console.log("Happy ho!idays!")
-}
-
-function happyHolidaysTo(name) {
-  return 'Happy Holidays, ${name}!'
+  return "Happy ho!idays!"
 }
